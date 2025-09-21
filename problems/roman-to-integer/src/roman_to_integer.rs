2d38296f12d3ffe7df61e783e02ef6677717d249
GaborZeller <gabriel.d.celery@gmail.com>
2025-09-21 22:39:59 +0100

    refactor: changed the way the roman integer problem is solved

--- problems/roman-to-integer/src/roman_to_integer.rs
+++ problems/roman-to-integer/src/roman_to_integer.rs
@@ -1,48 +1,41 @@
-use std::{collections::HashMap, panic};
+use std::panic;
 
 pub struct Solution {}
 
 impl Solution {
-    fn get_value_of_roman(s: &str) -> i32 {
-        let m: HashMap<&str, i32> = HashMap::from([
-            ("I", 1),
-            ("V", 5),
-            ("X", 10),
-            ("L", 50),
-            ("C", 100),
-            ("D", 500),
-            ("M", 1000),
-        ]);
-
-        match m.get(s) {
-            Some(v) => *v,
-            None => panic!("Could not find value for {s}"),
+    fn to_int(s: char) -> i32 {
+        match s {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => panic!("Could not find value for {s}"),
         }
     }
 
     pub fn roman_to_int(s: String) -> i32 {
-        let mut pointer: usize = 0;
         let mut total: i32 = 0;
 
-        while pointer < s.len() {
-            if pointer == s.len() - 1 {
-                total += Solution::get_value_of_roman(&s[pointer..(pointer + 1)]);
-                break;
-            }
+        let mut it = s.chars().peekable();
 
-            let curr = Solution::get_value_of_roman(&s[pointer..(pointer + 1)]);
-            let next = Solution::get_value_of_roman(&s[(pointer + 1)..(pointer + 2)]);
-
-            if curr < next {
-                total -= curr;
-                total += next;
-                pointer += 2;
+        loop {
+            if let Some(curr) = it.next() {
+                if let Some(next) = it.peek() {
+                    let curr_int = Solution::to_int(curr);
+                    let next_int = Solution::to_int(*next);
+                    if curr_int < next_int {
+                        total = total - curr_int + next_int;
+                        it.next();
+                        continue;
+                    }
+                }
+                total += Solution::to_int(curr);
                 continue;
             }
-
-            total += curr;
-            pointer += 1;
-            continue;
+            break;
         }
 
         return total;
