a716599f947ac7bb72d628195996489c2d9f330b
GaborZeller <gabriel.d.celery@gmail.com>
2025-10-05 09:16:13 +0100

    feat: first stab at sudoku solver

--- problems/sudoku-solver/src/solve_sudoku.rs
+++ problems/sudoku-solver/src/solve_sudoku.rs
@@ -8,9 +8,97 @@ struct Board {
 }
 
 impl Board {
+    fn new(board: &mut Vec<Vec<char>>) -> Self {
+        let mut cells = Vec::new();
+        for row in 0..9 {
+            let mut cell_row = Vec::new();
+            for col in 0..9 {
+                let cell = if board[row][col] == '.' {
+                    Cell {
+                        candidates: vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+                        solution: 0,
+                    }
+                } else {
+                    Cell {
+                        candidates: vec![],
+                        solution: board[row][col].to_digit(10).unwrap() as u8,
+                    }
+                };
+                cell_row.push(cell);
+            }
+            cells.push(cell_row);
+        }
+        Board { cells }
+    }
+
+    fn solve(&mut self) {
+        loop {
+            let hashed_before_cycle = self.hash();
+            for row_idx in 0..9 {
+                for col_idx in 0..9 {
+                    if self.is_cell_solved(row_idx, col_idx) {
+                        continue;
+                    }
+
+                    self.eliminate(row_idx, col_idx, self.get_all_solved_for_row(row_idx));
+                    self.eliminate(row_idx, col_idx, self.get_all_solved_for_col(row_idx));
+                    self.eliminate(
+                        row_idx,
+                        col_idx,
+                        self.get_all_solved_for_box(row_idx, col_idx),
+                    );
+
+                    self.set_cell_to_solved_if_can(row_idx, col_idx);
+                }
+            }
+            let hashed_after_cycle = self.hash();
+            if hashed_before_cycle == hashed_after_cycle {
+                break;
+            }
+        }
+    }
+
+    fn is_cell_solved(&self, row_idx: usize, col_idx: usize) -> bool {
+        let cell = &self.cells[row_idx][col_idx];
+        return cell.solution != 0;
+    }
+
+    fn set_cell_to_solved_if_can(&mut self, row_idx: usize, col_idx: usize) -> bool {
+        let cell = &mut self.cells[row_idx][col_idx];
+        if cell.candidates.len() > 1 {
+            return false;
+        }
+        cell.solution = cell.candidates[0];
+        return true;
+    }
+
+    fn eliminate(&mut self, row_idx: usize, col_idx: usize, nums_to_eliminate: Vec<u8>) {
+        let cell = &mut self.cells[row_idx][col_idx];
+        let new_candidates: Vec<u8> = cell
+            .candidates
+            .iter()
+            .filter(|&candidate| !nums_to_eliminate.contains(candidate))
+            .copied()
+            .collect();
+        cell.candidates = new_candidates;
+    }
+
+    fn hash(&self) -> String {
+        self.cells
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .map(|x| x.solution.to_string())
+                    .collect::<Vec<_>>()
+                    .join("")
+            })
+            .collect::<Vec<_>>()
+            .join("")
+    }
+
     fn get_all_solved_for_row(&self, row_idx: usize) -> Vec<u8> {
         let mut solved = vec![];
-        for cell in self.cells.get(row_idx).unwrap() {
+        for cell in &self.cells[row_idx] {
             if cell.solution == 0 {
                 continue;
             }
@@ -22,7 +110,7 @@ impl Board {
     fn get_all_solved_for_col(&self, col_idx: usize) -> Vec<u8> {
         let mut solved = vec![];
         for row in self.cells.iter() {
-            let cell = row.get(col_idx).unwrap();
+            let cell = &row[col_idx];
             if cell.solution == 0 {
                 continue;
             }
@@ -33,12 +121,9 @@ impl Board {
 
     fn get_all_solved_for_box(&self, row_idx: usize, col_idx: usize) -> Vec<u8> {
         let mut solved = vec![];
-        let offset_x = row_idx / 3;
-        let offset_y = col_idx / 3;
-        for y in 0..3 {
-            for x in 0..3 {
-                let row = self.cells.get(offset_x + x).unwrap();
-                let cell = row.get(offset_y + y).unwrap();
+        for x in 0..3 {
+            for y in 0..3 {
+                let cell = &self.cells[row_idx / 3 + x][col_idx / 3 + y];
                 if cell.solution == 0 {
                     continue;
                 }
@@ -52,7 +137,20 @@ impl Board {
 struct Solution {}
 
 impl Solution {
-    pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {}
+    pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
+        let mut sudoku_board = Board::new(board);
+        sudoku_board.solve();
+
+        // Update the original board with the solved values
+        for row_idx in 0..9 {
+            for col_idx in 0..9 {
+                if sudoku_board.cells[row_idx][col_idx].solution != 0 {
+                    board[row_idx][col_idx] =
+                        (sudoku_board.cells[row_idx][col_idx].solution + b'0') as char;
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
